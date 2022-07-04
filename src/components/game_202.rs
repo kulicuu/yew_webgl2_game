5bc01dc245e8eca99eaa39d367336fdb14c14e9b
J Wylie Cullick <wylie@terebinth.is>
2022-07-04 13:29:44 -0500

    ...

--- src/components/game_202.rs
+++ src/components/game_202.rs
@@ -14,13 +14,67 @@ use cgmath::Rad;
 
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::time;
-use std::time::{Duration, Instant};
+use std::time::*;
+// use std::time::{Duration, Instant};
+use std::convert::{TryInto};
+use std::ops::{Add, Sub, AddAssign, SubAssign};
 
 use gloo_console::log;
 use std::f32::consts::PI;
 
 const AMORTIZATION: f32 = 0.95;
+
+#[cfg(not(target_arch = "wasm32"))]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Instant(std::time::Instant);
+
+#[cfg(not(target_arch = "wasm32"))]
+impl Instant {
+    pub fn now() -> Self { Self(std::time::Instant::now()) }
+    pub fn duration_since(&self, earlier: Instant) -> Duration { self.0.duration_since(earlier.0) }
+    pub fn elapsed(&self) -> Duration { self.0.elapsed() }
+    pub fn checked_add(&self, duration: Duration) -> Option<Self> { self.0.checked_add(duration).map(|i| Self(i)) }
+    pub fn checked_sub(&self, duration: Duration) -> Option<Self> { self.0.checked_sub(duration).map(|i| Self(i)) }
+}
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(inline_js = r#"
+export function performance_now() {
+  return performance.now();
+}"#)]
+extern "C" {
+    fn performance_now() -> f64;
+}
+
+#[cfg(target_arch = "wasm32")]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Instant(u64);
+
+#[cfg(target_arch = "wasm32")]
+impl Instant {
+    pub fn now() -> Self { Self((performance_now() * 1000.0) as u64) }
+    pub fn duration_since(&self, earlier: Instant) -> Duration { Duration::from_micros(self.0 - earlier.0) }
+    pub fn elapsed(&self) -> Duration { Self::now().duration_since(*self) }
+    pub fn checked_add(&self, duration: Duration) -> Option<Self> {
+        match duration.as_micros().try_into() {
+            Ok(duration) => self.0.checked_add(duration).map(|i| Self(i)),
+            Err(_) => None,
+        }
+    }
+    pub fn checked_sub(&self, duration: Duration) -> Option<Self> {
+        match duration.as_micros().try_into() {
+            Ok(duration) => self.0.checked_sub(duration).map(|i| Self(i)),
+            Err(_) => None,
+        }
+    }
+}
+
+impl Add<Duration> for Instant { type Output = Instant; fn add(self, other: Duration) -> Instant { self.checked_add(other).unwrap() } }
+impl Sub<Duration> for Instant { type Output = Instant; fn sub(self, other: Duration) -> Instant { self.checked_sub(other).unwrap() } }
+impl Sub<Instant>  for Instant { type Output = Duration; fn sub(self, other: Instant) -> Duration { self.duration_since(other) } }
+impl AddAssign<Duration> for Instant { fn add_assign(&mut self, other: Duration) { *self = *self + other; } }
+impl SubAssign<Duration> for Instant { fn sub_assign(&mut self, other: Duration) { *self = *self - other; } }
+
 
 pub enum Msg {}
 
@@ -127,7 +181,6 @@ impl GameTwo {
                 velocity_dy: 0.0,
         }));
 
-
         let torpedos_vec : Rc<RefCell<Vec<Vehicle_100>>> = Rc::new(RefCell::new(vec![]));
     
         {
@@ -191,9 +244,7 @@ impl GameTwo {
                             velocity_dx: tsv_dx,
                             velocity_dy: tsv_dy,
                         };
-
-                        // torpedos_vec.borrow_mut().push(torpedo);
-      
+                        torpedos_vec.borrow_mut().push(torpedo);
                     }
                     _ => (),
                 }
